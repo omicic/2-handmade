3f5cca6cfeb554bb25cc7cdf85b5dc89de89ef8b
olivera <omicic@gmail.com>
2021-03-14 11:35:47 +0100

    welcome

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php include "partials/header.php"?>
-<h1>welcome</h1>
+
+<section class="welcome ">
+    <article class="container">
+        <h2>Welcome to my website</h2>
+        <hr>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur ipsa asperiores architecto cumque iste
+            dolore sapiente quod eius animi corrupti. Ut, ratione sunt. Praesentium quam ea explicabo suscipit expedita
+            iure
+            accusamus unde eligendi. Expedita sint ab sed saepe iste, impedit cupiditate a eaque ratione commodi optio
+            aspernatur perferendis! Nobis, pariatur.</p>
+    </article>
+
+</section>
+
 <?php include "partials/footer.php"?>
-    
