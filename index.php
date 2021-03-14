65a447c9151d4ad39e37b9bd14d8b7830fac0f2b
olivera <omicic@gmail.com>
2021-03-14 11:58:16 +0100

    whatcanIdo

--- index.php
+++ index.php
@@ -10,7 +10,34 @@
             accusamus unde eligendi. Expedita sint ab sed saepe iste, impedit cupiditate a eaque ratione commodi optio
             aspernatur perferendis! Nobis, pariatur.</p>
     </article>
+</section>
 
+<section class="whatcanIdo container">
+    <h3>What can I do for you?</h3>
+    <hr>
+    <article>
+        <div>
+            <h4>Wooden toys</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptate minus amet alias dignissimos,
+                quasi mollitia, odit libero optio facilis provident similique! Odit, possimus quidem reprehenderit rem
+                dolores, optio consequuntur itaque mollitia nisi animi dolor, unde quaerat nobis sequi recusandae.</p>
+            <button>read more</button>
+        </div>
+        <div>
+            <h4>Crismas</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptate minus amet alias dignissimos,
+                quasi mollitia, odit libero optio facilis provident similique! Odit, possimus quidem reprehenderit rem
+                dolores, optio consequuntur itaque mollitia nisi animi dolor, unde quaerat nobis sequi recusandae.</p>
+            <button>read more</button>
+        </div>
+        <div>
+            <h4>Origami</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam voluptate minus amet alias dignissimos,
+                quasi mollitia, odit libero optio facilis provident similique! Odit, possimus quidem reprehenderit rem
+                dolores, optio consequuntur itaque mollitia nisi animi dolor, unde quaerat nobis sequi recusandae.</p>
+            <button>read more</button>
+        </div>
+    </article>
 </section>
 
 <?php include "partials/footer.php"?>
